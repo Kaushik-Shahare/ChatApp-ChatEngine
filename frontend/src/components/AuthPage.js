787e62e954d029c106a6d8c74235c4e03f15846c
Kaushik-Shahare <kaushikshahare0@gmail.com>
2024-05-03 18:47:31 +0530

    Updated frontend-backend connection

--- frontend/src/components/AuthPage.js
+++ frontend/src/components/AuthPage.js
@@ -5,7 +5,9 @@ const AuthPage = (props) => {
     e.preventDefault();
     const { value } = e.target[0];
     axios
-      .post("http://localhost:3001/authenticate", { username: value })
+      .post("https://chat-app-chat-engine-three.vercel.app/authenticate", {
+        username: value,
+      })
       .then((r) => {
         props.onAuth({ ...r.data, secret: value });
       })
